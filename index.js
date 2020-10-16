608d9cf4b9066035f3e468110a032c8d6a09ed62
YourtionGuo <yourtion@gmail.com>
2020-10-16 23:11:08 +0800

    fix: update index

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const os = require('os');
-
 const addon = os.platform() === "darwin" ? require("./libs/mac") : require("./libs/linux");
 
 function encrypt(key, data) {
+  if(!key || !data) {
+    throw new Error("params error");
+  }
   return addon.crypt(0, key, data);
 }
+
 function decrypt(key, data) {
+  if(!key || !data) {
+    throw new Error("params error");
+  }
   return addon.crypt(1, key, data);
 }
 
